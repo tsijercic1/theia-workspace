47c0a986b51a5e0a9767ef1300d97eaf79b4bf05
Tarik Sijerčić <tsijercic1@etf.unsa.ba>
2020-04-20 14:41:53 +0200

    Some config

--- Zadatak2/main.c
+++ Zadatak2/main.c
@@ -1,6 +1,23 @@
 #include<stdio.h>
 
 int main(){
-    printf("helloworld")
+    printf("helloworld");
     return 0;
 }
+
+    // "@theia/core": "next",
+    // "@theia/filesystem": "next",
+    // "@theia/languages": "next",
+    // "@theia/monaco": "next",
+    // "@theia/process": "next",
+    // "@theia/workspace": "next",
+    // "@theia/callhierarchy": "next",
+    // "@theia/file-search": "next",
+    // "@theia/json": "1.0.0",
+    // "@theia/vscode-builtin-json-language-features": "0.2.1",
+    // "@theia/markers": "next",
+    // "@theia/messages": "next",
+    // "@theia/outline-view": "next",
+    // "@theia/plugin-ext-vscode": "next",
+    // "@theia/preferences": "next",
+    // "@theia/preview": "next",
